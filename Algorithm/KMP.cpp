7a3f1e763d4349a52a70004de845b67d54be7a8c
Lee Byeong Cheol <bc7817@naver.com>
2022-05-22 18:47:03 +0900

    Create KMP.cpp

---
+++ Algorithm/KMP.cpp
@@ -0,0 +1,40 @@
+//O(H)
+vector<int> getPartialMatch(const string& N) {
+    int m = N.size();
+    vector<int> pi(m, 0);
+    int begin = 1, matched = 0;
+    while(begin + matched < m) {
+        if(N[begin + matched] == N[matched]) {
+            matched++;
+            pi[begin + matched - 1] = matched;
+        } else {
+            if(matched == 0) begin++;
+            else {
+                begin += matched - pi[matched - 1];
+                matched = pi[matched - 1];
+            }
+        }
+    }
+    return pi;
+}
+
+//O(N)
+vector<int> kmpSearch(const string& H, const string& N) {
+    int n = H.size(), m = N.size();
+    vector<int> ret;
+    vector<int> pi = getPartialMatch(N);
+    int begin = 0, matched = 0;
+    while(begin <= n - m) {
+        if(matched < m && H[begin + matched] == N[matched]) {
+            matched++;
+            if(matched == m) ret.push_back(begin);
+        } else {
+            if(matched == 0) begin++;
+            else {
+                begin += matched - pi[matched - 1];
+                matched = pi[matched - 1];
+            }
+        }
+    }
+    return ret;
+}
