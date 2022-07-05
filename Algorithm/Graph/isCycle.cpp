65fc13840d74f7c4357544b4f7da8b8090e36f3e
Lee Byeong Cheol <bc7817@naver.com>
2022-07-05 16:23:43 +0900

    Update isCycle.cpp

--- Algorithm/Graph/isCycle.cpp
+++ Algorithm/Graph/isCycle.cpp
@@ -1,14 +1,16 @@
-
-int visited[100001];
-int finished[100001];
-bool cycle = false;
+int visited[501];
+int finished[501];
 
 //cycle 검출 : 역방향 간선 존재 확인  
-void dfs(int here) {
+bool dfs(int here) {
+    bool ret = false;
+    
     visited[here] = 1;
-    for(int there : e[here]) {
-        if(!visited[there]) dfs(there);
-        else if(!finished[there]) cycle = true;
+    for(int there : v[here]) {
+        if(!visited[there]) ret |= dfs(there);
+        else if(!finished[there]) ret = true;
     }
     finished[here] = 1;
+    
+    return ret;
 }
