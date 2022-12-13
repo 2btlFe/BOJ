e391ea390d80cd5265b81f78fff9eb181694e17e
Lee Byeong Cheol <bc7817@naver.com>
2022-12-13 16:36:18 +0900

    Seive

---
+++ Algorithm/Math/Seive.cpp
@@ -0,0 +1,16 @@
+ const int N = 1e6 + 6;
+vector<int> isprime(N, 1); 
+vector<int> sprime;
+ 
+void seive(int n) {
+    isprime[0] = isprime[1] = 0;
+    //sprime.push_back(0);
+    for(int i = 2; i <= n; i++) {
+        if(isprime[i]) {
+            sprime.push_back(i);
+            for(int j = i*i; j <= n; j+=i) {
+                isprime[j]=0;
+            }
+        }
+    }
+}
